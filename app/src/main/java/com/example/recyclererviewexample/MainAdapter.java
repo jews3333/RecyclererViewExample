52d57722573729c879fb68772e34853b14ea24d5
jews3333 <jews333333@gmail.com>
2019-06-09 20:51:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/recyclererviewexample/MainAdapter.java
@@ -0,0 +1,86 @@
+package com.example.recyclererviewexample;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.CustomViewHolder> {
+
+    private ArrayList<MainData> arrayList;
+
+    public MainAdapter(ArrayList<MainData> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        CustomViewHolder holder = new CustomViewHolder(view);
+
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MainAdapter.CustomViewHolder holder, int position) {
+        holder.iv_profile.setImageResource(arrayList.get(position).getIv_profile());
+        holder.tv_name.setText(arrayList.get(position).getTv_name());
+        holder.tv_content.setText(arrayList.get(position).getTv_content());
+
+        holder.itemView.setTag(position);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String curName = holder.tv_name.getText().toString();
+                Toast.makeText(view.getContext(), curName, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                remove(holder.getAdapterPosition());
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public void remove(int i){
+        try{
+            arrayList.remove(i);
+            notifyItemRemoved(i);
+        } catch (IndexOutOfBoundsException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        protected ImageView iv_profile;
+        protected TextView tv_name;
+        protected TextView tv_content;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.iv_profile = (ImageView) itemView.findViewById(R.id.iv_profile);
+            this.tv_name = (TextView) itemView.findViewById(R.id.tv_name);
+            this.tv_content = (TextView) itemView.findViewById(R.id.tv_content);
+        }
+    }
+}
